8d1a7dbd479d2e99d4bbb69ae4e900f9d216dc64
Daniel Cohen <codewithdan@daniels-mbp-2.lan>
2016-09-28 10:15:52 -0400

    Initial Commit

---
+++ hello world 4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello world 4
+//
+//  Created by Daniel on 9/28/16.
+//  Copyright © 2016 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
